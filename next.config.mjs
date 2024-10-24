a56b5a381a0fdbb11cb9c455c2a98adc0be1fc4f
hamster1963 <1410514192@qq.com>
2024-10-24 18:04:43 +0800

    fix: remove files

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,6 @@
-import withPWAInit from "@ducanh2912/next-pwa";
-import withBundleAnalyzer from "@next/bundle-analyzer";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-});
-
 const withNextIntl = createNextIntlPlugin();
-
-const withPWA = withPWAInit({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  disable: false,
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -30,4 +13,5 @@ const nextConfig = {
     },
   },
 };
-export default bundleAnalyzer(withPWA(withNextIntl(nextConfig)));
+
+export default withPWA(withNextIntl(nextConfig));
